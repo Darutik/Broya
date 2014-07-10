7f4dafa7a462f4d067ff60cd42540a00265919f9
Darutik <darutik@gmail.com>
2014-07-10 10:00:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
 Ans = ""
-Name ""
+Name = ""
+XBucle = False
 
 print ""
 print " Darutik presents..."
@@ -22,8 +23,7 @@ print ""
 print ""
 print ""
 raw_input("")
-print " When something lik ==> apear, you must answer the system."
-print " If you don't know what to say, type ? and you will be illuminated."
+print " When something like ==> apear, you must select a number."
 raw_input("")
 print ""
 
@@ -36,5 +36,71 @@ print " He-hello...?"
 raw_input("")
 print "SOMEONE:"
 print " Are you OK?"
+print ""
+print " 1. Yes..."
+print " 2. Who are you?"
+print " 3. I'm dead..."
 Ans = raw_input("==> ")
-if Ans == "?
+While XBucle != True:
+    if Ans == "1":
+        print " YOU:"
+        print " Yes..."
+        raw_input("")
+        print " SOMEONE:"
+        print " Sure?"
+        raw_input("")
+        print " YOU:"
+        print " Yes, I'm right..."
+        raw_input("")
+        print " SOMEONE:"
+        print " What's your name?"
+        Name = raw_input("==> ")
+        print " " + Name + ":"
+        print " I'm... " + Name + "."
+        print " People call me " + Name + "..."
+        raw_input("")
+        print " SOMEONE:"
+        print " Okay, " + Name + ". I'm Kevin, and I found you near the river, in the East."
+        print " You were unconscious, so I let you in my house, inside the forest."
+        XBucle = True
+    elif Ans == "2":
+        print " YOU:"
+        print " Who are you?"
+        raw_input("")
+        print " SOMEONE:"
+        print " I'm Kevin, and I found you near the river, in the East."
+        print " You were unconscious, so I let you in my house, inside the forest."
+        raw_input("")
+        print " KEVIN:"
+        print " So, you got it. Now let me ask you..."
+        print " How I call you?"
+        Name = raw_input("==> ")
+        print " " + Name + ":"
+        print " I'm... " + Name + "."
+        print " People call me " + Name + "..."
+        XBucle = True
+    elif Ans == "3":
+        print " YOU:"
+        print " I'm dead..."
+        raw_input("")
+        print " SOMEONE:"
+        print " Yes, and I am Bill Gates."
+        print " So, how I should call you, ghost? Casper???"
+        Name = raw_input("==> ")
+        print " " + Name + ":"
+        print " I'm... " + Name + "."
+        raw_input("")
+        print " SOMEONE:"
+        print " Sorry, " + Name + ", I'm not Bill Gates."
+        raw_input("")
+        print " SOMEONE:"
+        print " I'm Kevin, and I found you near the river, in the East."
+        print " You were unconscious, so I let you in my house, inside the forest."
+        XBucle = True
+    else:
+        print " SYSTEM:"
+        print " I don't like what you are saying..."
+        raw_input("")
+        XBucle = False
+XBucle = False
+raw_input("")
