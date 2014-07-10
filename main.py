01c7df77d399fd6390326e0ab208ab0d6707f737
Darutik <darutik@gmail.com>
2014-07-10 09:20:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+
+Ans = ""
+Name ""
+
+print ""
+print " Darutik presents..."
+print ""
+print " 01000010 01110010 01101111 01111001 01100001"
+print " More knowed as..."
+print ""
+print ""
+print ""
+print " :::::::::  :::::::::   ::::::::  :::   :::   :::     "
+print " :+:    :+: :+:    :+: :+:    :+: :+:   :+: :+: :+:   "
+print " +:+    +:+ +:+    +:+ +:+    +:+  +:+ +:+ +:+   +:+  "
+print " +#++:++#+  +#++:++#:  +#+    +:+   +#++: +#++:++#++: "
+print " +#+    +#+ +#+    +#+ +#+    +#+    +#+  +#+     +#+ "
+print " #+#    #+# #+#    #+# #+#    #+#    #+#  #+#     #+# "
+print " #########  ###    ###  ########     ###  ###     ### "
+print ""
+print ""
+print ""
+raw_input("")
+print " When something lik ==> apear, you must answer the system."
+print " If you don't know what to say, type ? and you will be illuminated."
+raw_input("")
+print ""
+
+print ""
+print " SOMEONE:"
+print " Hello."
+raw_input("")
+print " YOU:"
+print " He-hello...?"
+raw_input("")
+print "SOMEONE:"
+print " Are you OK?"
+Ans = raw_input("==> ")
+if Ans == "?
