bd2c02deb48235dea652ecde383438bb96a6ed06
Darutik <darutik@gmail.com>
2014-11-14 20:16:35 +0100

    Update Loop.py

--- Loop.py
+++ Loop.py
@@ -1,21 +1,20 @@
 # Porque soy muy vago, y hago Ctrl+C y luego Ctrl+V compulsivamente...
 
 while (XBucle != True):
-  Ans = raw_input("==> ")
-  if Ans == "1":
-    
-    
-    XBucle = True
-  elif Ans == "2":
-    
-    
-    XBucle = True
-  elif Ans == "3":
-    
-    
-    XBucle = True
-  else:
-    print " SYSTEM:"
-    print " I don't like what you are saying..."
-    raw_input("")
-    XBucle = False
+    Ans = raw_input("==> ")
+    if Ans == "1":
+        
+        XBucle = True
+    elif Ans == "2":
+        
+        
+        XBucle = True
+    elif Ans == "3":
+        
+        
+        XBucle = True
+    else:
+        print " SYSTEM:"
+        print " I don't like what you are saying..."
+        raw_input("")
+        XBucle = False
